88cead473b92a1c265fa56b6829ca3d4b8cdc67c
Lucas Rodríguez Valentino <lrodriguezv92@gmail.com>
2022-11-02 17:19:06 -0300

    ordeno el codigo

--- codigo.js
+++ codigo.js
@@ -1,55 +1,3 @@
-/* const equipos = [
-    {
-        id:1,
-        nombre:"AC Milan",
-        liga:"Serie A",
-        multiplicador:"8"
-    },
-    {
-        id:2,
-        nombre:"Paris Saint Germain",
-        liga:"Ligue 1",
-        multiplicador:"2.8"
-    },
-    {
-        id:3,
-        nombre:"Inter",
-        liga:"Serie A",
-        multiplicador:"9.3"
-    },
-    {
-        id:4,
-        nombre:"Real Madrid",
-        liga:"Liga Santander",
-        multiplicador:"5"
-    },
-    {
-        id:5,
-        nombre:"Barcelona",
-        liga:"Serie A",
-        multiplicador:"11.5"
-    },
-    {
-        id:6,
-        nombre:"Bayern Munich",
-        liga:"Bundesliga",
-        multiplicador:3.5
-    },
-    {
-        id:7,
-        nombre:"Manchester city",
-        liga:"Premier League",
-        multiplicador:"3"
-    },
-    {
-        id:8,
-        nombre:"Chelsea",
-        liga:"Premier League",
-        multiplicador:"6.7"
-    },
-]; */
-
-
 const productos = [
     {
         id:1,
@@ -89,7 +37,7 @@ mainjs.style.background="red";
 titulojs.innerText="Tienda";
 
 const carrito = [];
-let contenedor = document.getElementById("misprods");
+let contenedor = document.getElementById("prods");
 
 function creandoProductos(){
     for(const producto of productos){
@@ -106,7 +54,6 @@ function creandoProductos(){
         `;
     }
     productos.forEach((producto)=>{
-        //evento para cada boton
         document.getElementById(`btn${producto.id}`).addEventListener("click",function(){
             agregarAlCarrito(producto);
         });
@@ -129,43 +76,3 @@ function agregarAlCarrito(productoAComprar){
     let totalCarrito = carrito.reduce((acumulador,prod)=>acumulador+prod.precio,0);
     document.getElementById("total").innerText = "Total a pagar $: "+totalCarrito;
 }
-
-/* 
-let edad = prompt("Ingrese su edad");
-if (edad >= 18){
-    console.log("Bienvenido!");
-}
-else {
-    console.log("Pide ayuda a un adulto y continua.")
-}
-let usuario;
-let contrasenia;
-for (let i=1;i<=2;i++){
-    usuario=prompt("Ingresa usuario");
-    contrasenia=prompt("Ingresa contraseña para "+usuario);
-    
-    if((usuario=="Lucas")&&(contrasenia=="Rodriguez")){
-        console.log("Bienvenido al sistema!");
-        break;
-    }else{
-        console.log("Usuario y/o contraseña erroneos! Restan "+(2-i)+" intentos");
-    }
-} */
-
-/* let equipoElegido = prompt("Elige tu equipo");
-const equipoSel = equipos.find((equipo) => equipo.nombre == equipoElegido);
-if (equipoSel != undefined){
-    console.table(equipoSel);
-}else{
-    console.log("Ese equipo no está en champions league");
-}
-let apuesta=parseFloat(prompt("Ingresa apuesta"));
-function calcularGanancia(apuesta){
-    return apuesta * equipoSel.multiplicador;
-}
-let ganancia = calcularGanancia(apuesta);
-console.log("Si el equipo gana su ganancia es $"+ganancia);
-function mostrarSaldoFinal(apuesta,ganancia){
-    console.log("Si el equipo gana su saldo será $"+ (apuesta + ganancia));
-}
-mostrarSaldoFinal(apuesta,ganancia); */
---
+++ orden.js
@@ -0,0 +1,91 @@
+
+/* 
+let edad = prompt("Ingrese su edad");
+if (edad >= 18){
+    console.log("Bienvenido!");
+}
+else {
+    console.log("Pide ayuda a un adulto y continua.")
+}
+let usuario;
+let contrasenia;
+for (let i=1;i<=2;i++){
+    usuario=prompt("Ingresa usuario");
+    contrasenia=prompt("Ingresa contraseña para "+usuario);
+    
+    if((usuario=="Lucas")&&(contrasenia=="Rodriguez")){
+        console.log("Bienvenido al sistema!");
+        break;
+    }else{
+        console.log("Usuario y/o contraseña erroneos! Restan "+(2-i)+" intentos");
+    }
+} */
+
+/* let equipoElegido = prompt("Elige tu equipo");
+const equipoSel = equipos.find((equipo) => equipo.nombre == equipoElegido);
+if (equipoSel != undefined){
+    console.table(equipoSel);
+}else{
+    console.log("Ese equipo no está en champions league");
+}
+let apuesta=parseFloat(prompt("Ingresa apuesta"));
+function calcularGanancia(apuesta){
+    return apuesta * equipoSel.multiplicador;
+}
+let ganancia = calcularGanancia(apuesta);
+console.log("Si el equipo gana su ganancia es $"+ganancia);
+function mostrarSaldoFinal(apuesta,ganancia){
+    console.log("Si el equipo gana su saldo será $"+ (apuesta + ganancia));
+}
+mostrarSaldoFinal(apuesta,ganancia); */
+
+/* const equipos = [
+    {
+        id:1,
+        nombre:"AC Milan",
+        liga:"Serie A",
+        multiplicador:"8"
+    },
+    {
+        id:2,
+        nombre:"Paris Saint Germain",
+        liga:"Ligue 1",
+        multiplicador:"2.8"
+    },
+    {
+        id:3,
+        nombre:"Inter",
+        liga:"Serie A",
+        multiplicador:"9.3"
+    },
+    {
+        id:4,
+        nombre:"Real Madrid",
+        liga:"Liga Santander",
+        multiplicador:"5"
+    },
+    {
+        id:5,
+        nombre:"Barcelona",
+        liga:"Serie A",
+        multiplicador:"11.5"
+    },
+    {
+        id:6,
+        nombre:"Bayern Munich",
+        liga:"Bundesliga",
+        multiplicador:3.5
+    },
+    {
+        id:7,
+        nombre:"Manchester city",
+        liga:"Premier League",
+        multiplicador:"3"
+    },
+    {
+        id:8,
+        nombre:"Chelsea",
+        liga:"Premier League",
+        multiplicador:"6.7"
+    },
+]; */
